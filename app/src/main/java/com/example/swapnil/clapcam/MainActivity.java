dfe5a6fef7421944965ba9daad8b5295fc936a01
swapbot0908 <swapbot0908@github.com>
2018-12-04 00:33:37 -0500

    Camera2API

--- app/src/main/java/com/example/swapnil/clapcam/MainActivity.java
+++ app/src/main/java/com/example/swapnil/clapcam/MainActivity.java
@@ -82,3 +82,127 @@ public class MainActivity extends AppCompatActivity {
         ORIENTATIONS.append(Surface.ROTATION_270,180);
 
     }
+
+    private String cameraId;
+    private CameraDevice cameraDevice;
+    private CameraCaptureSession cameraCaptureSessions;
+    private CaptureRequest.Builder captureRequestBuilder;
+    private Size imageDimension;
+    private ImageReader imageReader;
+
+    private File file;
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+    private boolean mFlashSupported;
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
+
+    CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            cameraDevice = camera;
+            createCameraPreview();
+
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+            //Toast.makeText(getApplicationContext(), "quitting....", Toast.LENGTH_LONG).show();
+            //cameraDevice.close();
+            //openCamera();
+
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice cameraDevice, int i) {
+            Toast.makeText(getApplicationContext(), "error....", Toast.LENGTH_LONG).show();
+            cameraDevice.close();
+            cameraDevice = null;
+
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textureView = (TextureView)findViewById(R.id.textureView);
+        assert textureView != null;
+        textureView.setSurfaceTextureListener(textureListener);
+        btnCapture = (Button)findViewById(R.id.btnCapture);
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
+
+        Start = (Button)findViewById(R.id.start_reg);
+        Speech = (TextView)findViewById(R.id.speech);
+
+        Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isConnected()){
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    startActivityForResult(intent, REQUEST_CODE);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Plese Connect to Internet", Toast.LENGTH_LONG).show();
+                }}
+
+        });
+    }
+
+    public  boolean isConnected()
+    {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo net = cm.getActiveNetworkInfo();
+        if (net!=null && net.isAvailable() && net.isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            super.onActivityResult(requestCode, resultCode, data);
+            switch (requestCode) {
+                case REQUEST_CODE: {
+                    if (resultCode == RESULT_OK && null != data) {
+                        ArrayList<String> voiceInText = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                        if (voiceInText.get(0).equals("cheese")) {
+
+//                            new CountDownTimer(20000, 1000) {
+//
+//                                public void onTick(long millisUntilFinished) {
+//                                    Start.setText("seconds remaining: " +new SimpleDateFormat("ss").format(new Date( millisUntilFinished)));
+//                                }
+//
+//                                public void onFinish() {
+//                                    Start.setText("Start Speech Recognition");
+//                                }
+//                            }.start();
+
+                            //Button btn  = (Button) findViewById(R.id.btnCapture);
+                            btnCapture.performClick();
+                            
+                        } else {
+                            Speech.setText(voiceInText.get(0));
+                            Toast.makeText(getApplicationContext(), "Not the Keyword", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    break;
+                }
+            }
+
+        }
+        //super.onActivityResult(requestCode, resultCode, data);
+    }
+
