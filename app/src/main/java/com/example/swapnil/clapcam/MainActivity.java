ae51e4fe8caac9d2869ce28ff52d1681188edef1
swapbot0908 <swapbot0908@github.com>
2018-12-04 00:25:34 -0500

    Camera2API

--- app/src/main/java/com/example/swapnil/clapcam/MainActivity.java
+++ app/src/main/java/com/example/swapnil/clapcam/MainActivity.java
@@ -1,142 +1,84 @@
 package com.example.swapnil.clapcam;
 
-import android.app.Activity;
+import android.Manifest;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.graphics.Camera;
+import android.graphics.ImageFormat;
+import android.graphics.Paint;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.icu.text.SimpleDateFormat;
+import android.icu.text.UnicodeSetSpanner;
+import android.media.Image;
+import android.media.ImageReader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.aware.Characteristics;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.speech.RecognizerIntent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.os.Bundle;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.provider.MediaStore;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.os.CountDownTimer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import android.widget.ListView;
+import android.app.Dialog;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Button cam_button;
-    ImageView iv;
+    private Button btnCapture;
+    private TextureView textureView;
+    private static final int REQUEST_CODE = 1234;
+    Button Start;
+    TextView Speech;
+    Dialog match_text_dialog;
+    ListView textlist;
+    ArrayList<String> matches_text;
 
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static{
+        ORIENTATIONS.append(Surface.ROTATION_0,90);
+        ORIENTATIONS.append(Surface.ROTATION_90,0);
+        ORIENTATIONS.append(Surface.ROTATION_180,270);
+        ORIENTATIONS.append(Surface.ROTATION_270,180);
+
     }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //---------------------------------------------------
-        cam_button = (Button) findViewById(R.id.cam_button);
-        iv = (ImageView) findViewById(R.id.imageView);
-
-        cam_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                //if video select ACTION_IMAGE_VIDEO
-                startActivityForResult(intent,0);
-            }
-        });
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-    //--------------------------------------------------------------------------
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-
-        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-        iv.setImageBitmap(bitmap);
-    }
-    //------------------------------------------------------------------------
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-	//-----------------------------------------------------------------------
-	
-	
-	public class TakePictureActivity extends Activity {
-    private static String TAG = "TakePictureActivity";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate: ");
-        Intent intent = getIntent();
-        if (intent == null) {
-            finish();
-        } else if (CameraActivity.needPermissions(this)) {
-            startActivity(new Intent(this, CameraActivity.class)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-            finish();
-            return;
-        }
-        else if (!isVoiceInteraction()) {
-            Log.e(TAG, "Not voice interaction");
-            if (intent != null) {
-                intent.setComponent(null);
-                intent.setPackage("com.google.android.GoogleCamera");
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-            finish();
-            return;
-            setContentView(R.layout.activity_camera);
-            CameraFragment fragment = CameraFragment.newInstance();
-            fragment.setArguments(getIntent().getExtras());
-
-            getFragmentManager().beginTransaction()
-                    .replace(R.id.container, fragment)
-                    .commit();
-        }
-
-        private static final int PERMISSIONS_REQUEST_ALL_PERMISSIONS = 1;
-        private Bundle mSavedInstanceState;
-
-        protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-//---------------------------------------------------------------------
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}
