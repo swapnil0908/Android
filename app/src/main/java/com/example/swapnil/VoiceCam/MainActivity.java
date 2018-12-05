85aff9678a2b2a15e41d6592e75665b924359ccb
swapbot0908 <swapbot0908@github.com>
2018-12-04 20:22:45 -0500

    Timer Implementation with camera switch option

--- app/src/main/java/com/example/swapnil/VoiceCam/MainActivity.java
+++ app/src/main/java/com/example/swapnil/VoiceCam/MainActivity.java
@@ -24,24 +24,32 @@ import android.media.Image;
 import android.media.ImageReader;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.net.wifi.aware.Characteristics;
+import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.provider.Settings;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Size;
 import android.util.SparseIntArray;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -60,6 +68,8 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.Semaphore;
+
 import android.widget.ListView;
 import android.app.Dialog;
 
@@ -68,7 +78,7 @@ import android.app.Dialog;
 public class MainActivity extends AppCompatActivity {
 
     private Button btnCapture;
-    private Button cambtn;
+    private Button camswitch;
     private TextureView textureView;
     private static final int REQUEST_CODE = 1234;
     Button Start;
@@ -86,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //private String cameraId;
+    private String cameraId;
     private CameraDevice cameraDevice;
     private CameraCaptureSession cameraCaptureSessions;
     private CaptureRequest.Builder captureRequestBuilder;
@@ -99,15 +109,12 @@ public class MainActivity extends AppCompatActivity {
     private Handler mBackgroundHandler;
     private HandlerThread mBackgroundThread;
 
-
-    public static final String CAMERA_FRONT = "1";
-    public static final String CAMERA_BACK = "0";
-
-    private String cameraId = CAMERA_BACK;
+    private Semaphore mCameraOpenCloseLock = new Semaphore(1);
 
 
-
-
+    EditText editText;
+    SpeechRecognizer mSpeechRecognizer;
+    Intent mSpeechRecognizerIntent;
 
     CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
         @Override
@@ -150,60 +157,86 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        cambtn = (Button)findViewById(R.id.button_switch_camera);
-//        // Listener for Switch cameras button
-//        cambtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //switchCameras();
-//            }
-//        });
-
-        Start = (Button)findViewById(R.id.start_reg);
-        Speech = (TextView)findViewById(R.id.speech);
-
-        Start.setOnClickListener(new View.OnClickListener() {
+        camswitch = (Button)findViewById(R.id.camswitch);
+        // Listener for Switch cameras button
+        camswitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isConnected()){
-                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                    startActivityForResult(intent, REQUEST_CODE);
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Plese Connect to Internet", Toast.LENGTH_LONG).show();
-                }}
-
+                switchCameras();
+            }
         });
-    }
 
-    public  boolean isConnected()
-    {
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo net = cm.getActiveNetworkInfo();
-        if (net!=null && net.isAvailable() && net.isConnected()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
-            super.onActivityResult(requestCode, resultCode, data);
-            switch (requestCode) {
-                case REQUEST_CODE: {
-                    if (resultCode == RESULT_OK && null != data) {
-                        ArrayList<String> voiceInText = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                        if (voiceInText.get(0).equals("cheese")) {
+//        Start = (Button)findViewById(R.id.start_reg);
+//        Speech = (TextView)findViewById(R.id.speech);
 
-                            new CountDownTimer(10000, 1000) {
+//        Start.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(isConnected()){
+//                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+//                            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//                    startActivityForResult(intent, REQUEST_CODE);
+//                }
+//                else{
+//                    Toast.makeText(getApplicationContext(), "Plese Connect to Internet", Toast.LENGTH_LONG).show();
+//                }}
+//
+//        });
+        checkPermission();
+        editText = findViewById(R.id.editText);
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
 
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                if (matches != null) {
+                    editText.setText(matches.get(0));
+                    if(matches.get(0).equals( "cheese")) {
+                        //Toast.makeText(MainActivity.this, "IDHAR!", Toast.LENGTH_SHORT).show();
+                        editText.setText(matches.get(0));
+                        new CountDownTimer(10000, 1000) {
+//
                                 public void onTick(long millisUntilFinished) {
                                     //Start.setText("seconds remaining: " +new SimpleDateFormat("ss").format(new Date( millisUntilFinished)));
-                                    Toast.makeText(getApplicationContext(), "Photo in : " + millisUntilFinished/1000, Toast.LENGTH_SHORT).show();
+                                    //Toast.makeText(getApplicationContext(), "Photo in : " + millisUntilFinished/1000, Toast.LENGTH_SHORT).show();
+                                    Toast toast1 = Toast.makeText(MainActivity.this, "Photo in : " + millisUntilFinished/1000, Toast.LENGTH_SHORT);
+                                    toast1.setGravity(Gravity.CENTER, 0, 0);
+                                    toast1.show();
                                     //Toast.setGravity(Gravity.CENTER, 0, 0);
 
                                 }
@@ -217,27 +250,147 @@ public class MainActivity extends AppCompatActivity {
                                     btnCapture.performClick();
                                 }
                             }.start();
-
-                            //Button btn  = (Button) findViewById(R.id.btnCapture);
-
-
-//                            takePicture();
-                            //Speech.setText("Camera");
-                            //Toast.makeText(getApplicationContext(), "Taking Picture", Toast.LENGTH_LONG).show();
-
-
-                        } else {
-                            Speech.setText(voiceInText.get(0));
-                            Toast.makeText(getApplicationContext(), "Not the Keyword", Toast.LENGTH_LONG).show();
-                        }
                     }
-                    break;
                 }
             }
 
-        }
-        //super.onActivityResult(requestCode, resultCode, data);
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+        findViewById(R.id.btn).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+
+                    case MotionEvent.ACTION_UP:
+                        mSpeechRecognizer.stopListening();
+                        editText.setHint("you will see the input here");
+                        break;
+
+                    case MotionEvent.ACTION_DOWN:
+                        editText.setText("");
+                        editText.setHint("Listening....");
+                        mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+                        break;
+                }
+                return false;
+            }
+        });
+
     }
+
+    private void  switchCameras() {
+        mCameraOpenCloseLock.release();
+        cameraDevice.close();
+        CameraManager manager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String cameraId = manager.getCameraIdList()[0];
+            if (cameraId.equals("0")) {
+                cameraId = "1";
+                Toast.makeText(MainActivity.this, "switch mara!" + cameraId, Toast.LENGTH_SHORT).show();
+            }
+            else {
+                cameraId = "0";
+                Toast.makeText(MainActivity.this, "switch mara ye le!" + cameraId, Toast.LENGTH_SHORT).show();
+            }
+            try {
+                if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                {
+                    ActivityCompat.requestPermissions(this, new String[]{
+                            Manifest.permission.CAMERA,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    },REQUEST_CAMERA_PERMISSION);
+                    return;
+                }
+                manager.openCamera(cameraId,stateCallback,null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        } catch (CameraAccessException e) {
+            Toast.makeText(this, "Cannot access the camera.", Toast.LENGTH_SHORT).show();
+            this.finish();
+        }
+    }
+
+    private void checkPermission(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(!(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)){
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+                finish();
+            }
+        }
+    }
+//    public  boolean isConnected()
+////    {
+////        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+////        NetworkInfo net = cm.getActiveNetworkInfo();
+////        if (net!=null && net.isAvailable() && net.isConnected()) {
+////            return true;
+////        } else {
+////            return false;
+////        }
+////    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+//            super.onActivityResult(requestCode, resultCode, data);
+//            switch (requestCode) {
+//                case REQUEST_CODE: {
+//                    if (resultCode == RESULT_OK && null != data) {
+//                        ArrayList<String> voiceInText = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//                        if (voiceInText.get(0).equals("cheese")) {
+//
+//                            new CountDownTimer(10000, 1000) {
+//
+//                                public void onTick(long millisUntilFinished) {
+//                                    //Start.setText("seconds remaining: " +new SimpleDateFormat("ss").format(new Date( millisUntilFinished)));
+//                                    //Toast.makeText(getApplicationContext(), "Photo in : " + millisUntilFinished/1000, Toast.LENGTH_SHORT).show();
+//                                    Toast toast1 = Toast.makeText(MainActivity.this, "Photo in : " + millisUntilFinished/1000, Toast.LENGTH_SHORT);
+//                                    toast1.setGravity(Gravity.CENTER, 0, 0);
+//                                    toast1.show();
+//                                    //Toast.setGravity(Gravity.CENTER, 0, 0);
+//
+//                                }
+//
+//
+//                                public void onFinish() {
+//                                    //Start.setText("Start Speech Recognition");
+//                                    Toast toast = Toast.makeText(MainActivity.this,"CLICKED", Toast.LENGTH_LONG);
+//                                    toast.setGravity(Gravity.CENTER, 0, 0);
+//                                    toast.show();
+//                                    btnCapture.performClick();
+//                                }
+//                            }.start();
+//
+//                            //Button btn  = (Button) findViewById(R.id.btnCapture);
+//
+//
+////                            takePicture();
+//                            //Speech.setText("Camera");
+//                            //Toast.makeText(getApplicationContext(), "Taking Picture", Toast.LENGTH_LONG).show();
+//
+//
+//                        } else {
+//                            Speech.setText(voiceInText.get(0));
+//                            Toast.makeText(getApplicationContext(), "Not the Keyword", Toast.LENGTH_LONG).show();
+//                        }
+//                    }
+//                    break;
+//                }
+//            }
+//
+//        }
+//        //super.onActivityResult(requestCode, resultCode, data);
+//    }
 
 //    public void switchCameras() {
 //        if (cameraId.equals(CAMERA_FRONT)) {
@@ -422,7 +575,7 @@ public class MainActivity extends AppCompatActivity {
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
-        }
+    }
 
 
     TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
